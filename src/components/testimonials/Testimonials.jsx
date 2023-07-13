7a6ab60084bb3bdec77e5834043dd2174c5c87dc
Radhikasuresh <radhikasuresh550@gmail.com>
2023-07-13 13:46:17 +0530

    project added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,6 +8,61 @@ export default function Testimonials() {
         <h2 className="project">Projects</h2>
       </div>
       <div class="testimonials">
+      <div class="container">
+          <div class="banner-img10"></div>
+          <h1 class="name" style={{ color: "rgb(44, 73, 167)" }}>
+           Zen Class Query Resolve-Clone
+          </h1>
+          <div className="btn-cont">
+            <button
+              class="btn"
+              onClick={() =>
+                window.open("https://github.com/Radhikasuresh/ticketing-system-frontend")
+              }
+            >
+              Github FE <IoLogoGithub />
+            </button>
+            <button
+              class="btn"
+              onClick={() => window.open("https://peaceful-cranachan-00a32d.netlify.app/")}
+            >
+              View Site <IoMdOpen />
+            </button>
+            <button
+              class="btn"
+              onClick={() =>
+                window.open("https://github.com/Radhikasuresh/ticketingSystemBackend")
+              }
+            >
+              Github BE <IoLogoGithub />
+            </button>
+          </div>
+          <p class="description">
+            This is Fullstack MERN website made it as a Zen Class Query Resolve-Clone.<br></br>This
+            application is used to get the queries resolved.
+          </p>
+          <p>
+            <span
+              style={{
+                color: "rgb(65, 201, 255)",
+                fontWeight: "500",
+                fontSize: "0.7rem"
+              }}
+            >
+              TECH USED :
+            </span>{" "}
+            <span
+              style={{
+                color: "#d18d0d",
+                fontWeight: "bold",
+                fontSize: "0.7rem",
+              }}
+            >
+              {" "}
+              REACT, NODE, EXPRESS JS, MONGODB
+            </span>
+          </p>
+        </div>
         <div class="container">
           <div class="banner-img1"></div>
           <h1 class="name" style={{ color: "crimson" }}>
@@ -116,21 +171,21 @@ export default function Testimonials() {
             <button
               class="btn"
               onClick={() =>
-                window.open("https://github.com/Thiru-cse/Explorer-Frontend")
+                window.open("https://github.com/Radhikasuresh/url-shortener-frontend")
               }
             >
               Github FE <IoLogoGithub />
             </button>
             <button
               class="btn"
-              onClick={() => window.open("https://explorer-thiru.vercel.app/")}
+              onClick={() => window.open("https://free-url-shortener.netlify.app/")}
             >
               View Site <IoMdOpen />
             </button>
             <button
               class="btn"
               onClick={() =>
-                window.open("https://github.com/Thiru-cse/Explorer-Backend")
+                window.open("https://github.com/Radhikasuresh/url-shortener-backend")
               }
             >
               Github BE <IoLogoGithub />
@@ -172,7 +227,7 @@ export default function Testimonials() {
             <button
               class="btn"
               onClick={() =>
-                window.open("https://github.com/Thiru-cse/Vchat-Frontend")
+                window.open("https://github.com/Radhikasuresh/domcalc")
               }
             >
               Github FE <IoLogoGithub />
@@ -212,54 +267,7 @@ export default function Testimonials() {
           </p>
         </div>
 
-        {/* <div class="container">
-          <div class="banner-img3"></div>
-          <h1 class="name" style={{ color: "rgb(255, 65, 65)" }}>
-            YouTube Clone
-          </h1>
-          <div className="btn-cont">
-            <button
-              class="btn"
-              onClick={() => window.open("")}
-            >
-              Github <IoLogoGithub />
-            </button>
-            <button
-              class="btn"
-              onClick={() =>
-                window.open("https://thiru-youtube-clone.vercel.app/")
-              }
-            >
-              View Site <IoMdOpen />
-            </button>
-          </div>
-          <p class="description">
-            This website made it as a Youtube.<br></br>This application is used
-            to watch videos.
-          </p>
-          <p>
-            <span
-              style={{
-                color: "rgb(65, 201, 255)",
-                fontWeight: "500",
-                fontSize: "0.8rem"
-              }}
-            >
-              TECH USED :
-            </span>{" "}
-            <span
-              style={{
-                color: "#d18d0d",
-                fontWeight: "bold",
-                fontSize: "0.8rem",
-              }}
-            >
-              HTML, CSS, JAVASCRIPT
-            </span>
-          </p>
-        </div> */}
-
-        <div class="container">
+    <div class="container">
           <div class="banner-img4"></div>
           <h1 class="name" style={{ color: "rgb(218, 181, 181)" }}>
             Weather Tracker
